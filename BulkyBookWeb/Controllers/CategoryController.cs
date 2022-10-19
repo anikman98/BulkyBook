7e898b00e68b85a17607247ee77b8c4664fc8f59
anikman <anikman98@gmail.com>
2022-10-19 23:48:43 +0530

    Created create post method

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -23,5 +23,14 @@ namespace BulkyBookWeb.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges(); 
+            return RedirectToAction("Index");
+        }
     }
 }
